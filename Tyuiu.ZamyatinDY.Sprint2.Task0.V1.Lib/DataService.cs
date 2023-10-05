d80e1e00c3d26b4e32a0e0b96ca83b147e8415fe
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-10-05 16:29:32 +0500

    Add project files.

---
+++ Tyuiu.ZamyatinDY.Sprint2.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ZamyatinDY.Sprint2.Task0.V1.Lib
+{
+    public class DataService : ISprint2Task0V1
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y + 111;
+            res[1] = x != y + 111;
+            res[2] = x < y + 112;
+            res[3] = x > y + 114;
+            res[4] = x <= y + 113;
+            res[5] = x >= y + 114;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint2.Task0.V1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZamyatinDY.Sprint2.Task0.V1.Lib;
+namespace Tyuiu.ZamyatinDY.Sprint2.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            int x = 233;
+            int y = 122;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Замятин Д. Ю. | ИИПб 23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПб 23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+            Console.WriteLine("* которая вернет логическую последовательность: (T, F, T, F, T, F)        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
