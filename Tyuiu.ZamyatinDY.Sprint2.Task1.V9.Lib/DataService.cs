c138aff65fa9857df4837c76314a3415f50accaa
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-10-05 17:23:46 +0500

    Task1

---
+++ Tyuiu.ZamyatinDY.Sprint2.Task1.V9.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ZamyatinDY.Sprint2.Task1.V9.Lib
+{
+    public class DataService : ISprint2Task1V9
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c > d); // 11 > 335 - false, 174 > 478 - false | False
+            res[1] = (a < b) & (c > d); // 11 < 335 - true, 174 > 478 - false | False
+            res[2] = (a < b) || (c > d); // 11 < 335 - true, 174 > 478 - false | True
+            res[3] = (a < b) && (c > d); // 11 < 335 - true, 174 > 478 - false | False
+            res[4] = !res[0]; // True
+            res[5] = (a > b) ^ (d < c); // 11 > 335 - false, 478 < 174 - false | True
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint2.Task1.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZamyatinDY.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZamyatinDY.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b666ed0-87f3-4c1b-84e6-a4c704740682")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
