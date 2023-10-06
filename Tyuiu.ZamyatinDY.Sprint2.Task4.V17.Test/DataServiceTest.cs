2b04f29306e2cfac4d8d83abe08f1c02fca05c6d
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-10-06 20:35:54 +0500

    Task4

---
+++ Tyuiu.ZamyatinDY.Sprint2.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+using Tyuiu.ZamyatinDY.Sprint2.Task4.V17.Lib;
+
+namespace Tyuiu.ZamyatinDY.Sprint2.Task4.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            
+            double x = 5;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 4.480;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()   
+        {
+            DataService ds = new DataService();
+
+            double x = 25;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 1298.767;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint2.Task4.V17/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZamyatinDY.Sprint2.Task4.V17.Lib;
+
+namespace Tyuiu.ZamyatinDY.Sprint2.Task4.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Замятин Д. Ю. | ИИПб 23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПб 23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с использова-  *");
+            Console.WriteLine("* нием тернарного оператора, где пользователь вводит значения x,y         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " +  res);
+
+            Console.ReadKey();
+        }
+    }
+}
